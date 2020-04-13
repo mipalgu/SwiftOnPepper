a1cbb2660070ff1d19f9e455aa5f172cdfbc9970
Callum McColl <callum.mccoll1@gmail.com>
2020-04-13 16:59:14 +1000

    Added command line args to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,28 @@
 #! /bin/sh
 
-docker image build --build-arg SSH_USER=`whoami` --build-arg GIT_USERS_NAME="`git config user.name`" --build-arg GIT_USERS_EMAIL="`git config user.email`" --build-arg SWIFTVER="5.2" --build-arg PARALLEL=1 -t mipal-pepper-swift-crosstoolchain-build .
+PARALLEL=1
+SWIFT_VERSION=5.2
+
+usage() { echo "Usage: $0 [-j<value>] -s <swift-version>"; }
+
+while getopts "j:hls:t:" o; do
+    case "${o}" in
+        h)
+            usage
+            exit 0
+            ;;
+        j)
+            PARALLEL=${OPTARG}
+            ;;
+        s)
+	        SWIFT_VERSION=${OPTARG}
+	        ;;
+        *)
+            echo "Invalid argument ${o}"
+            usage 1>&2
+            exit 1
+            ;;
+    esac
+done
+
+docker image build --build-arg SSH_USER=`whoami` --build-arg GIT_USERS_NAME="`git config user.name`" --build-arg GIT_USERS_EMAIL="`git config user.email`" --build-arg SWIFTVER="$SWIFT_VERSION" --build-arg PARALLEL=$PARALLEL -t mipal-pepper-swift-crosstoolchain-build .
