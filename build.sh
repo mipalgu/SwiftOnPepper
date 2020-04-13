fbfda1bc2e76d0c3d83f262994f41408ee6d9077
Callum McColl <callum.mccoll1@gmail.com>
2020-04-13 17:01:32 +1000

    Added -u option to build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
+set -e
 
 PARALLEL=1
 SWIFT_VERSION=5.2
+SSH_USERNAME=`whoami`
 
-usage() { echo "Usage: $0 [-j<value>] -s <swift-version>"; }
+usage() { echo "Usage: $0 [-j<value>] -s <swift-version> -u <ssh_username>"; }
 
 while getopts "j:hls:t:" o; do
     case "${o}" in
@@ -17,6 +19,9 @@ while getopts "j:hls:t:" o; do
         s)
 	        SWIFT_VERSION=${OPTARG}
 	        ;;
+        u)
+            SSH_USERNAME=${OPTARG}
+            ;;
         *)
             echo "Invalid argument ${o}"
             usage 1>&2
@@ -25,4 +30,4 @@ while getopts "j:hls:t:" o; do
     esac
 done
 
-docker image build --build-arg SSH_USER=`whoami` --build-arg GIT_USERS_NAME="`git config user.name`" --build-arg GIT_USERS_EMAIL="`git config user.email`" --build-arg SWIFTVER="$SWIFT_VERSION" --build-arg PARALLEL=$PARALLEL -t mipal-pepper-swift-crosstoolchain-build .
+docker image build --build-arg SSH_USER="$SSH_USERNAME" --build-arg GIT_USERS_NAME="`git config user.name`" --build-arg GIT_USERS_EMAIL="`git config user.email`" --build-arg SWIFTVER="$SWIFT_VERSION" --build-arg PARALLEL=$PARALLEL -t mipal-pepper-swift-crosstoolchain-build .
